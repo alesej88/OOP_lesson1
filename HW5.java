3ab542941b3c57b28e7063aadc3f8bd598faba78
aleksey <alesej88@ya.ru>
2021-03-26 22:01:20 +0600

    OOP_lesson1

---
+++ HW5.java
@@ -0,0 +1,41 @@
+public class HW5 {
+
+    static class Employee {
+        int age;
+        String name;
+        String surname;
+        long tel;
+        String email;
+        double salary;
+
+
+        public static void main(String[] args) {
+            Employee emp = new Employee();
+            emp.age = 30;
+            emp.name = "Ivan";
+            emp.surname = "Petrov";
+            emp.tel = 89003332220l;
+            emp.email = "ip@gmail.com";
+            emp.salary = 30000;
+            System.out.println("Возраст сотрудника: " + emp.age + " лет" + " " + "Имя: " + emp.name + " " + "Фамилия: " + emp.surname + " " + "телефон: " + emp.tel + " " + "емайл: " + emp.email + " " + "зарплата: " + emp.salary);
+
+
+        }
+    }
+
+}
+//lesson2 ⦁	Создать массив из 5 сотрудников
+
+class persArray{
+    Person[] persArray = new Person[5];
+    persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+    persArray[1] = new Person ("Vasya Petrov", "Director", "Vp@gmail.com", "89254517744", 95000, 45);
+    persArray[2] = new Person("Evgeniy Sidorov", "kom.director", "Evgeniy33@mail.ru", 80000, 60);
+    persArray[3] = new Person("Oksana Fadina", "gl.buh", "OkF@ya.ru", 75000, 57);
+    persArray[4] = new
+    for()
+
+    }
+
+
+}
